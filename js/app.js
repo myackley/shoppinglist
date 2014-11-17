470b63926ac8eabb5980a91b08b06e85616d5f5d
Mark Yackley <myackley@hotmail.com>
2014-11-16 21:33:36 -0600

    Added psuedo code for jsand commented out sample list items

--- js/app.js
+++ js/app.js
@@ -5,4 +5,28 @@ $(document).ready(function() {
 			minimumResultsForSearch: -1
 		});
 
+	// add item
+
+	// 		grab input values
+
+	//		format values into list item
+
+	//		append to shopapp_list ul
+
+	// mark as "purchased"
+
+	//		traverse to find this item in dom
+
+	//		apply class to show as "purchased"
+
+	//		remove the class if marked as "purchased" by accident
+
+	// remove from list
+
+	//		traverse to find this item in dom
+
+	//		store the item
+
+	//		append the item to shopapp_panel-viewremoved ul
+
 });
