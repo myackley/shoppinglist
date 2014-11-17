19fce6a56d30512b1f07e0d56743cef14a2f1016
Mark Yackley <myackley@hotmail.com>
2014-11-16 22:49:08 -0600

    Removed Select2 for now, added functionality for adding items to the Items list along with appropriate validation and the ability to clear form values

--- js/app.js
+++ js/app.js
@@ -1,17 +1,43 @@
 $(document).ready(function() {
 	
 	// init select2
-		$("#additem_qty").select2({
-			minimumResultsForSearch: -1
-		});
+	// $("#additem_qty").select2({
+	// 	minimumResultsForSearch: -1,
+	// 	placeholder: "1",
+	// 	initSelection: function(element, callback) {}
+	// });
+
+
 
 	// add item
+	var addItem = function() {
 
-	// 		grab input values
+		var itemName = $("#additem_item").val();
+		var itemQuantity = $("#additem_qty option:selected").val();
+		var listitem = "";
+		var itemBtns = "<div class=\"shopapp_list-btns\"><a href=\"#\" class=\"btn_icon shopapp_btn-check\"><span class=\"fa fa-check\"></span></a><a href=\"#\" class=\"btn_icon shopapp_btn-remove\"><span class=\"fa fa-close\"></span></a></div>"
 
-	//		format values into list item
+		if ( itemName != "" || itemName.length > 2 ) {
+			$(".valid_additem").css("display","none");
+			
+			//	format values into list item
+			listitem = "<li><div class=\"shopapp_list-item\"><p><span>"+itemQuantity+"</span>"+itemName+"</p></div>"+itemBtns+"</li>";
 
-	//		append to shopapp_list ul
+			//	append to shopapp_list ul
+			$(".shopapp_list ul").append(listitem);
+			
+		} else {
+			$(".valid_additem").css("display","block");
+		}
+
+	}
+
+
+	$(".btn_submit").on("click", addItem);
+	$(".btn_cancel").on("click", function() {
+		$("#additem_item").val("");
+		$("#additem_qty").val("1");
+	});
 
 	// mark as "purchased"
 
